f6b8c706085588bcce199a3dee39481ed5df1602
Rafael Blanco Cordero <rafabc1979@gmail.com>
2018-03-28 14:42:02 +0200

    initial up eureka archetype

---
+++ archetype-eureka/src/main/resources/archetype-resources/src/main/java/EurekaApp.java
@@ -0,0 +1,23 @@
+package ${package};
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+@RefreshScope
+public class EurekaApp {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("com.sun.xml.ws.transport.http.client.HttpTransportPipe.dump", "true");
+		System.setProperty("com.sun.xml.internal.ws.transport.http.client.HttpTransportPipe.dump", "true");
+		System.setProperty("com.sun.xml.ws.transport.http.HttpAdapter.dump", "true");
+		System.setProperty("com.sun.xml.internal.ws.transport.http.HttpAdapter.dump", "true");
+		
+		SpringApplication.run(EurekaApp.class, args);
+	}
+
+}
