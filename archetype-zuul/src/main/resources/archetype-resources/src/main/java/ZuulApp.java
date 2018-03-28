c834c9c3ee9a1456d5a2213cf3dd590e79590826
Rafael Blanco Cordero <rafabc1979@gmail.com>
2018-03-28 11:30:20 +0200

    initial up archetype zuul

---
+++ archetype-zuul/src/main/resources/archetype-resources/src/main/java/ZuulApp.java
@@ -0,0 +1,26 @@
+package ${package};
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+
+
+
+@SpringBootApplication
+@EnableZuulProxy
+@RefreshScope  //This annotation allow to zuul to detect changes in zuul.yml (from config server) and auto refresh the spring scope with this changes
+public class ZuulApp {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("com.sun.xml.ws.transport.http.client.HttpTransportPipe.dump", "true");
+		System.setProperty("com.sun.xml.internal.ws.transport.http.client.HttpTransportPipe.dump", "true");
+		System.setProperty("com.sun.xml.ws.transport.http.HttpAdapter.dump", "true");
+		System.setProperty("com.sun.xml.internal.ws.transport.http.HttpAdapter.dump", "true");
+		
+		SpringApplication.run(MicroserviceZuulApp.class, args);
+	}
+
+}
