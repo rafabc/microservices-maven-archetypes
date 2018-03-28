ac3b0a6406c0be0bedcd0306ee449843a1a35ebb
Rafael Blanco Cordero <rafabc1979@gmail.com>
2018-03-28 16:25:33 +0200

    initial up config server archetype

---
+++ archetype-config-server/src/main/resources/archetype-resources/src/main/java/ConfigServerApp.java
@@ -0,0 +1,26 @@
+package ${package};
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigServerApp {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("com.sun.xml.ws.transport.http.client.HttpTransportPipe.dump", "true");
+		System.setProperty("com.sun.xml.internal.ws.transport.http.client.HttpTransportPipe.dump", "true");
+		System.setProperty("com.sun.xml.ws.transport.http.HttpAdapter.dump", "true");
+		System.setProperty("com.sun.xml.internal.ws.transport.http.HttpAdapter.dump", "true");
+		
+		SpringApplication.run(ConfigServerApp.class, args);
+	}
+}
+
